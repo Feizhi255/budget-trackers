3fb3f29bc8b04262ff74e5156848e90f0e3cf6b2
Jordan Kirby <jrock255@hotmail.com>
2020-11-24 22:07:18 -0700

    added a service worker in public folder

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+// const fileCacheName = "file-v1"
+// const dataCacheName = "data-v1"
+
+// const fileToCache = [
+//     "/",
+//     "/index.html",
+//     "/style.css",
+//     "/index.js",
+//     "/favicon.ico",
+//     "/assets/image/icons/icon-192x192.png"
+// ]
